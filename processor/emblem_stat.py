098d9f45947776f0b7d373afbd74b9e2791c94e7
wings27 <wenqs27@gmail.com>
2017-02-15 00:27:49 +0800

    optimize freq_stat.

--- processor/emblem_stat.py
+++ processor/emblem_stat.py
@@ -1,5 +1,7 @@
 import logging
+import multiprocessing
 import operator
+from itertools import repeat
 
 from mapreduce.driver import MapReduceDriver
 from mapreduce.emblem_freq import EmblemFreq
@@ -14,8 +16,6 @@ logging.basicConfig(level=logging.INFO)
 
 
 def stat_freq():
-    freq_threshold = 1
-
     driver_freq = MapReduceDriver(EmblemFreq.map_fn, EmblemFreq.reduce_fn)
     emblem_list = Emblem(songci_list).emblem_list()
     emblem_freq_stat = driver_freq(emblem_list)
@@ -23,8 +23,20 @@ def stat_freq():
 
     total_len = len(emblem_freq_stat)
     logger.info('Total length of emblem_freq_stat is %d', total_len)
+
+    workers = (multiprocessing.cpu_count() or 1) * 4
+    pool = multiprocessing.Pool(processes=workers)
+    emblem_freq_chunks = MapReduceDriver.chunks(emblem_freq_stat, int(total_len / workers))
+    pool.starmap(save_freq_stat, zip(emblem_freq_chunks, repeat(total_len)))
+
+    data_source.create_index('emblem', 'name', unique=True)
+    data_source.create_index('emblem', 'freq_rate')
+
+
+def save_freq_stat(freq_stat, total_len):
+    freq_threshold = 1
     prev_freq = prev_freq_rate = 0  # cache previous quotient (a.k.a. freq) to improve performance
-    for emblem_freq in emblem_freq_stat:  # todo: optimize this using concurrency
+    for emblem_freq in freq_stat:
         emblem_name, freq = emblem_freq
         if freq <= freq_threshold:
             break
@@ -32,9 +44,6 @@ def stat_freq():
         data_source.save('emblem', {'name': emblem_name}, {'freq_rate': freq_rate})
         prev_freq = freq
         prev_freq_rate = freq_rate
-
-    data_source.create_index('emblem', 'name', unique=True)
-    data_source.create_index('emblem', 'freq_rate')
 
 
 if __name__ == '__main__':
