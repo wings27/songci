594441be27d4068e3e82466cf4948a60ed6f2563
wings27 <wenqs27@gmail.com>
2017-03-27 00:15:37 +0800

    extract dao from processor.

--- processor/emblem_processor.py
+++ processor/emblem_processor.py
@@ -1,15 +1,12 @@
 import logging
-import multiprocessing
 import operator
-from itertools import repeat
-
-import pymongo
+import os
 
 from mapreduce.driver import MapReduceDriver
 from mapreduce.emblem_finals import EmblemFinals
 from mapreduce.emblem_freq import EmblemFreq
 from nlp.emblem import Emblem
-from processor.data_source import MongoDataSource
+from processor.songci_dao import MongoDAO
 
 
 class EmblemProcessor:
@@ -17,14 +14,10 @@ class EmblemProcessor:
     Processor that deals with emblems.
 
     It is normally used to extract emblems from a list of songci contents,
-    and then save them to the data source,
+    and then save them back to the data accessing object,
     along with some other fields such as the term-frequency of emblems.
 
-    The data source is supposed be schema-free (e.g. MongoDB),
-    so the schema of emblem is defined, or rather described, within this class
-    in order to provide flexibility.
-
-    The schema of emblem:
+    The schema of an emblem:
         - name
         - freq_rate
         - finals
@@ -32,28 +25,41 @@ class EmblemProcessor:
             - rhyme
             - tones
     """
-    COLLECTION_EMBLEM = 'emblem'
-    COLLECTION_SONGCI_CONTENT = 'songci_content'
 
-    data_source = MongoDataSource()
-
-    logger = logging.getLogger('emblem_stat')
+    logger = logging.getLogger('EmblemProcessor')
     logging.basicConfig(level=logging.INFO)
+
+    def __init__(self, emblem_dao):
+        self.emblem_dao = emblem_dao
+        self.songci_list = emblem_dao.load_songci_list()
+
+        if 'load_emblem_list' in dir(emblem_dao):
+            loaded_emblem_list = emblem_dao.load_emblem_list()
+            if loaded_emblem_list:
+                self.emblem_list = loaded_emblem_list
+            else:
+                self._gen_emblem_list()
+        else:
+            self._gen_emblem_list()
+
+    def _gen_emblem_list(self):
+        self.emblem_list = [pair[0] for pair in self.gen_freq_rate()]
 
     def gen_freq_rate(self):
         """
-        This function does two things:
-        1. Extract emblems from a list of songci contents.
-        2. Generate field of freq_rate for those emblems.
+        Generate frequency rates for emblems.
         The field of freq_rate is the term-frequency rate of an emblem,
         whose value defines whether a word is an emblem.
 
         :return: list of tuples(emblem_name, freq_rate)
         """
-        map_reduce_driver = MapReduceDriver(EmblemFreq.map_fn, EmblemFreq.reduce_fn)
-        songci_list = self.data_source.find(self.COLLECTION_SONGCI_CONTENT)
+        self.logger.info(
+            'Generating frequency rates, total=%d', len(self.emblem_list))
 
-        emblem_stat_list = map_reduce_driver(Emblem(songci_list).emblem_list())
+        raw_emblem_list = Emblem(self.songci_list).emblem_list()
+        map_reduce_driver = MapReduceDriver(
+            EmblemFreq.map_fn, EmblemFreq.reduce_fn)
+        emblem_stat_list = map_reduce_driver(raw_emblem_list)
         emblem_stat_list.sort(key=operator.itemgetter(1), reverse=True)
 
         def map_to_freq_rate(freq_stat_list):
@@ -61,12 +67,14 @@ class EmblemProcessor:
             total_len = len(freq_stat_list)
 
             ret = []
-            prev_freq = prev_freq_rate = 0  # cache previous quotient (a.k.a. freq) to improve performance
+            # cache previous quotient (a.k.a. freq) to improve performance
+            prev_freq_rate = prev_freq = 0
             for freq_stat in freq_stat_list:
                 name, freq = freq_stat
                 if freq < min_freq_allowed:
                     break
-                freq_rate = prev_freq_rate if freq == prev_freq else freq / total_len
+                freq_rate = prev_freq_rate if freq == prev_freq \
+                    else freq / total_len
                 ret.append((name, freq_rate))
 
                 prev_freq = freq
@@ -79,17 +87,17 @@ class EmblemProcessor:
 
     def gen_finals(self):
         """
-        Generate field of finals for emblems from self.data_source,
-        where finals is a dict whose keys are pinyin, rhyme, tones, etc.
+        Generate finals for emblems.
+        Finals are dictionaries whose keys are pinyin, rhyme, tones, etc.
 
         :return: list of tuples(emblem_name, finals)
         """
-        map_reduce_driver = MapReduceDriver(EmblemFinals.map_fn, EmblemFinals.reduce_fn, workers=4)
-        emblem_list = self.data_source.find(
-            self.COLLECTION_EMBLEM,
-            projection=['name'],
-            sort=[('freq_rate', pymongo.DESCENDING)])
-        emblem_finals_stat = map_reduce_driver((emblem['name'] for emblem in emblem_list))
+        self.logger.info(
+            'Generating finals, total=%d', len(self.emblem_list))
+        workers = 4 * (os.cpu_count() or 1)
+        map_reduce_driver = MapReduceDriver(
+            EmblemFinals.map_fn, EmblemFinals.reduce_fn, workers=workers)
+        emblem_finals_stat = map_reduce_driver(self.emblem_list)
 
         result_to_be_saved = [(name, {
             'pinyin': finals.pinyin,
@@ -99,39 +107,10 @@ class EmblemProcessor:
 
         return result_to_be_saved
 
-    def save_emblems_field(self, emblem_with_field_list, field_name, index=True):
-        """
-        Save emblems along with provided field,
-        where field can be any of the types that self.data_source supports.
-
-        :param emblem_with_field_list: tuple of (emblem_name, field)
-        :param field_name: the name of that field
-
-        :return: None
-        """
-        total_len = len(emblem_with_field_list)
-        self.logger.info('Saving field [%s], total=%d', field_name, total_len)
-
-        workers = (multiprocessing.cpu_count() or 1) * 4
-        emblem_freq_chunks = MapReduceDriver.chunks(emblem_with_field_list, int(total_len / workers))
-
-        with multiprocessing.Pool(processes=workers) as pool:
-            pool.starmap(self._save_emblems_field, zip(emblem_freq_chunks, repeat(field_name)))
-
-        if index:
-            self.data_source.create_index(self.COLLECTION_EMBLEM, 'name', unique=True)
-            self.data_source.create_index(self.COLLECTION_EMBLEM, field_name)
-            field = emblem_with_field_list[0][1]
-            if isinstance(field, dict):
-                for key in field.keys():
-                    self.data_source.create_index(self.COLLECTION_EMBLEM, field_name + '.' + key)
-
-    def _save_emblems_field(self, emblem_with_field_list, field_name):
-        for (emblem_name, field) in emblem_with_field_list:
-            self.data_source.save(self.COLLECTION_EMBLEM, {'name': emblem_name}, {field_name: field})
-
 
 if __name__ == '__main__':
-    processor = EmblemProcessor()
-    processor.save_emblems_field(processor.gen_freq_rate(), 'freq_rate')
-    processor.save_emblems_field(processor.gen_finals(), 'finals')
+    mongo_dao = MongoDAO()
+    processor = EmblemProcessor(mongo_dao)
+
+    mongo_dao.save_emblems_field(processor.gen_freq_rate(), 'freq_rate')
+    mongo_dao.save_emblems_field(processor.gen_finals(), 'finals')
---
+++ processor/songci_dao.py
@@ -0,0 +1,68 @@
+import logging
+import multiprocessing
+from itertools import repeat
+
+import pymongo
+
+from mapreduce.driver import MapReduceDriver
+from processor.data_source import MongoDataSource
+
+
+class MongoDAO:
+    COLLECTION_EMBLEM = 'emblem'
+    COLLECTION_SONGCI_CONTENT = 'songci_content'
+
+    logger = logging.getLogger('MongoDAO')
+    logging.basicConfig(level=logging.INFO)
+
+    data_source = MongoDataSource()
+
+    def load_songci_list(self):
+        # todo change to read content.
+        return list(self.data_source.find(self.COLLECTION_SONGCI_CONTENT))
+
+    def load_emblem_list(self):
+        return [emblem['name'] for emblem in (self.data_source.find(
+            self.COLLECTION_EMBLEM,
+            projection=['name'], sort=[('freq_rate', pymongo.DESCENDING)]))]
+
+    def save_emblems_field(
+            self, emblem_with_field_list, field_name, index=True):
+        """
+        Save emblems along with provided field,
+        where field can be any of the types that self.data_source supports.
+
+        :param emblem_with_field_list: tuple of (emblem_name, field)
+        :param field_name: the name of that field
+        :param index: whether it is needed to create an index
+
+        :return: None
+        """
+        total_len = len(emblem_with_field_list)
+        self.logger.info('Saving field [%s], total=%d', field_name, total_len)
+
+        workers = 4 * (multiprocessing.cpu_count() or 1)
+        emblem_freq_chunks = MapReduceDriver.chunks(
+            emblem_with_field_list, int(total_len / workers))
+
+        with multiprocessing.Pool(processes=workers) as pool:
+            pool.starmap(
+                self._save_emblems_field,
+                zip(emblem_freq_chunks, repeat(field_name)))
+
+        if index:
+            self.data_source.create_index(
+                self.COLLECTION_EMBLEM, 'name', unique=True)
+            self.data_source.create_index(
+                self.COLLECTION_EMBLEM, field_name)
+            field = emblem_with_field_list[0][1]
+            if isinstance(field, dict):
+                for key in field.keys():
+                    self.data_source.create_index(
+                        self.COLLECTION_EMBLEM, field_name + '.' + key)
+
+    def _save_emblems_field(self, emblem_with_field_list, field_name):
+        for (emblem_name, field) in emblem_with_field_list:
+            self.data_source.save(
+                self.COLLECTION_EMBLEM, {'name': emblem_name},
+                {field_name: field})
