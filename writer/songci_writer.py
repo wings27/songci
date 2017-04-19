f7aeb47e0f03b2face6dfdbca121b36dbaf11915
wings27 <wenqs27@gmail.com>
2017-04-19 18:04:19 +0800

    refactor Writer and WordPattern

--- writer/songci_writer.py
+++ writer/songci_writer.py
@@ -1,4 +1,5 @@
 import logging
+import re
 from collections import deque
 from io import StringIO
 
@@ -13,22 +14,33 @@ class WordPattern:
         self.tones_regex = tones_regex
         self.rhyme = rhyme
 
-    def fetch_word(self) -> (str, int):
-        # todo fetch word by word_pattern
-        return self.tones_regex, self.length
+        self.logger = logging.getLogger(__name__)
+        self.logger.setLevel(logging.DEBUG)
+
+    def fetch_word(self, data_source_dao) -> (str, int):
+        where = {}
+        if self.tones_regex:
+            where["finals.tones"] = re.compile('^%s$' % self.tones_regex)
+        if self.rhyme:
+            where["finals.rhyme"] = self.rhyme
+
+        word = data_source_dao.random_emblem_name(where).next()['name']
+        self.logger.debug('fetch_word: (%s,%s) -> %s',
+                          self.tones_regex, self.rhyme, word)
+        return word, self.length
 
 
 class SongciWriter:
-    logger = logging.getLogger('processor.EmblemProcessor')
-    logging.basicConfig(level=logging.INFO)
-
-    def __init__(self, tune_name, rhyme) -> None:
+    def __init__(self, tune_name, rhyme, data_source_dao) -> None:
         self.tune_name = tune_name
         self.rhyme = rhyme
+        self.data_source_dao = data_source_dao
 
         self._tune = load_tune(self.tune_name)
         self._word_patterns = None
-        self._write_pointer = None
+
+        self.logger = logging.getLogger(__name__)
+        self.logger.setLevel(logging.INFO)
 
     def write_new(self) -> (str, str):
         if not self._tune.pattern:
@@ -39,14 +51,15 @@ class SongciWriter:
         self._analyze()
 
         with StringIO() as songci_io:
-            self._write_pointer = 0
-            while self._write_pointer < len(pattern):
-                char = pattern[self._write_pointer]
+            pointer = 0
+            while pointer < len(pattern):
+                char = pattern[pointer]
                 if char in pingze.ping_ze_chars.values():
-                    word, pointer_step = self._next_word_pattern().fetch_word()
+                    word, pointer_step = self._next_word_pattern().fetch_word(
+                        self.data_source_dao)
                 else:
                     word, pointer_step = char, len(char)
-                self._write_pointer += pointer_step
+                pointer += pointer_step
                 songci_io.write(word)
 
             return self._tune.title, songci_io.getvalue()
@@ -68,26 +81,26 @@ class SongciWriter:
             WordPattern(length=2, tones_regex='.(3|4)'),
             WordPattern(length=2, tones_regex='.(1|2)'),
             WordPattern(length=3, tones_regex='.(3|4)(1|2)', rhyme=rhyme),
-            WordPattern(length=2, tones_regex='.(3|4)'),
             WordPattern(length=2, tones_regex='.(1|2)'),
-            WordPattern(length=3, tones_regex='.(3|4)(1|2)', rhyme=rhyme),
             WordPattern(length=2, tones_regex='.(3|4)'),
+            WordPattern(length=3, tones_regex='(3|4)(1|2)(1|2)', rhyme=rhyme),
             WordPattern(length=2, tones_regex='.(1|2)'),
-            WordPattern(length=3, tones_regex='.(3|4)(1|2)', rhyme=rhyme),
+            WordPattern(length=2, tones_regex='.(3|4)'),
+            WordPattern(length=3, tones_regex='(3|4)(1|2)(1|2)', rhyme=rhyme),
 
             WordPattern(length=2, tones_regex='.(3|4)'),
             WordPattern(length=2, tones_regex='.(1|2)'),
-            WordPattern(length=3, tones_regex='.(3|4)(1|2)', rhyme=rhyme),
-            WordPattern(length=2, tones_regex='.(3|4)'),
+            WordPattern(length=3, tones_regex='(1|2)(3|4)(3|4)'),
             WordPattern(length=2, tones_regex='.(1|2)'),
-            WordPattern(length=3, tones_regex='.(3|4)(1|2)', rhyme=rhyme),
             WordPattern(length=2, tones_regex='.(3|4)'),
+            WordPattern(length=3, tones_regex='(3|4)(1|2)(1|2)', rhyme=rhyme),
             WordPattern(length=2, tones_regex='.(1|2)'),
-            WordPattern(length=3, tones_regex='.(3|4)(1|2)', rhyme=rhyme),
+            WordPattern(length=2, tones_regex='.(3|4)'),
+            WordPattern(length=3, tones_regex='(3|4)(1|2)(1|2)', rhyme=rhyme),
         ])
 
 
 if __name__ == '__main__':
-    songci_writer = SongciWriter(tune_name='huanxisha', rhyme='ang')
+    songci_writer = SongciWriter(tune_name='huanxisha', rhyme='an')
     title, content = songci_writer.write_new()
     print('new_songci: \n《%s》\n\n%s' % (title, content))
