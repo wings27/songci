1d63b5b26c3b063861d786d31b0f03795399df2e
wings27 <wenqs27@gmail.com>
2017-04-20 15:47:47 +0800

    adding functions to pingze

--- nlp/pingze.py
+++ nlp/pingze.py
@@ -5,3 +5,18 @@ ping_ze_chars = {
     'ZE_YUN': 'D',
     'ANY': 'X',
 }
+
+
+def is_yun(chars: str is not None) -> bool:
+    return chars.endswith('C') or chars.endswith('D')
+
+
+def to_tones_regex(chars: str is not None) -> str:
+    to_tones_regex_mapping = {
+        'A': '(1|2)',
+        'C': '(1|2)',
+        'B': '(3|4)',
+        'D': '(3|4)',
+        'X': '.',
+    }
+    return chars.translate(str.maketrans(to_tones_regex_mapping))
